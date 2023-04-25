d121b0cdaadb6db43a598c4dedaaeb4ba865a980
icoo08217 <icoo08216@gmail.com>
2023-04-25 19:41:54 +0900

    lombok 도입 , @RequiredArgsConstructor 도입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,21 +3,23 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // DI : 의존관계 주입 , 의존성 주입이라고 한다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired // 생성자가 딱 한 개인 경우에는 @Autowired를 삭제할 수 있다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // @Autowired // 생성자가 딱 한 개인 경우에는 @Autowired를 삭제할 수 있다.
+    //    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //        this.memberRepository = memberRepository;
+    //        this.discountPolicy = discountPolicy;
+    //    }
+    // 위의 코드는 @RequiredArgsConstructor 가 만들어준다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
